7a8fe6683cf564755ee9ccba1aae3aebea26c214
xt2005cc <christian2005cruz@hotmail.com>
2025-11-24 11:48:08 -0800

    Add lab 11/24

--- lab1124.cpp
+++ lab1124.cpp
@@ -9,6 +9,13 @@ struct Node{
     Node (int data): data(data), left(nullptr), right(nullptr){}
 };
 
+int treeHeight(Node *root){
+    if(root == nullptr) return -1;
+    if (root->left == nullptr && root->right == nullptr)
+    return 0;
+    return 1 + max(treeHeight(root->left), treeHeight(root->right));
+}
+
 void printlevel(Node *root, int level){
     if(root==nullptr) return;
     if(level == 0) cout << root->data << " ";
@@ -50,14 +57,25 @@ int main(){
     for (auto v: data){
         cout << "Inserting " << v << "\n";
         BSTinsert(root, v);
+        int depth = treeHeight(root);
+        for (int i = 0; i <+depth; i++){
+            printlevel(root,i);
+            cout << endl;
+        }
+    int target = 1;
+    Node *current = root; Node *parent = nullptr;
+    while(current != nullptr && current ->data != target){
+        if (target < current->data)
+            current = current->left;
+            else
+            current = current->right;
     }
-    cout << endl;
-    printlevel(root,0); cout << endl;
-    printlevel(root,1); cout << endl;
-    printlevel(root,2); cout << endl;
-    printlevel(root,3); cout << endl;
-
-
-
-
+    cout << "Found target " << current->data << "Parent " << parent->data << endl;
+    parent->left = nullptr;
+    delete current;
+    for (int i = 0; i <+depth; i++){
+        printlevel(root,i);
+        cout << endl;
+        }
+    }
 }
